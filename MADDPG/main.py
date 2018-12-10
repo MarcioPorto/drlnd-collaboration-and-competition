4883b16e4c92580e78c5dec83f88d15f2b3fbe0f
Marcio Porto <mflporto@gmail.com>
2018-12-09 19:58:35 -0500

    refactors training and testing helpers

---
+++ MADDPG/main.py
@@ -0,0 +1,123 @@
+from collections import deque
+
+import matplotlib.pyplot as plt
+import progressbar as pb
+
+
+def train(maddpg, n_episodes=1000, save_every=50):
+    widget = [
+        "Episode: ", pb.Counter(), '/' , str(n_episodes), ' ', 
+        pb.Percentage(), ' ', pb.ETA(), ' ', pb.Bar(marker=pb.RotatingMarker()), ' ', 
+        'Rolling Average: ', pb.FormatLabel('')
+    ]
+    timer = pb.ProgressBar(widgets=widget, maxval=n_episodes).start()
+
+    solved = False
+    scores_total = []
+    scores_deque = deque(maxlen=100)
+    rolling_score_averages = []
+    best_score = 0.0
+
+    for i_episode in range(1, n_episodes+1):
+        current_average = 0.0 if i_episode == 1 else rolling_score_averages[-1]
+        widget[12] = pb.FormatLabel(str(current_average)[:6])
+        timer.update(i_episode)
+
+        env_info = env.reset(train_mode=True)[brain_name]
+        states = env_info.vector_observations[:, -STATE_SIZE:]
+        scores = np.zeros(num_agents)
+        maddpg.reset()
+
+        while True:
+            actions = maddpg.act(states, add_noise=True)
+
+            env_info = env.step(actions)[brain_name]
+            next_states = env_info.vector_observations[:, -STATE_SIZE:]
+            rewards = env_info.rewards
+            dones = env_info.local_done
+            
+            maddpg.step(states, actions, rewards, next_states, dones)
+
+            scores += rewards
+            states = next_states
+
+            if np.any(dones):
+                break
+
+        max_episode_score = np.max(scores)
+
+        scores_deque.append(max_episode_score)
+        scores_total.append(max_episode_score)
+
+        average_score = np.mean(scores_deque)
+        rolling_score_averages.append(average_score)
+
+        if average_score > best_score:
+            best_score = average_score
+            if solved:
+                # This model is better than a previously saved model, so we'll save it
+                maddpg.save_model()
+
+        if average_score >= 0.5 and not solved:
+            print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(
+                i_episode, average_score
+            ))
+            solved = True
+            maddpg.save_model()
+
+        if i_episode % save_every == 0 and not solved:
+            maddpg.save_model()
+
+    maddpg.save_model()
+
+    return scores_total, rolling_score_averages
+
+
+def plot_results(scores, rolling_score_averages):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    plt.plot(np.arange(1, len(scores) + 1), scores, label="Max Score")
+    plt.plot(np.arange(1, len(rolling_score_averages) + 1), rolling_score_averages, label="Rolling Average")
+    plt.axhline(y=0.5, color="r", linestyle="-", label="Environment Solved")
+
+    plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+    plt.ylabel("Score")
+    plt.xlabel("Episode #")
+    plt.show()
+    
+
+def test(maddpg, env, num_games=11, load_model=False):
+    if load_model:
+        maddpg.load_model()
+    
+    print("Agent #0: Red racket")
+    print("Agent #1: Blue racket")
+    print("---------------------")
+    
+    game_scores = [0 for _ in range(NUM_AGENTS)]
+
+    for i in range(1, num_games+1):
+        env_info = env.reset(train_mode=False)[brain_name]   
+        states = env_info.vector_observations
+        scores = np.zeros(NUM_AGENTS)
+
+        while True:
+            actions = maddpg.act(states)
+
+            env_info = env.step(actions)[brain_name]
+            next_states = env_info.vector_observations
+            rewards = env_info.rewards
+            scores += rewards
+            dones = env_info.local_done
+
+            if np.any(dones):
+                winner = np.argmax(scores)
+                game_scores[winner] += 1
+                print("Partial game score: {}".format(game_scores))
+                break
+
+            states = next_states
+
+    print("---------------------")
+    print("Winner: Agent #{}".format(np.argmax(game_scores)))
